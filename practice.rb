8a17c4d313c87bd2efa638fbb9a47ffc84752727
Sebastian  Darena <sdseb2582@gmail.com>
2018-08-12 17:20:12 +0000

    solution to practice problem

--- practice.rb
+++ practice.rb
@@ -1,1 +1,27 @@
 # create a method, called print_array, that takes an argument of an array and uses #each to iterate through the array and puts each element.
+require "pry"
+array = ["jake", "jessie", "george", "sally"]
+
+def print_array(names) 
+  puts 
+  
+  # def print_array(array)
+  #   array.each {|element| puts element}
+  # end 
+  
+# github staging:
+# "committ stages"
+# git add. 
+
+
+
+
+
+
+
+
+
+
+
+
+binding.pry 
