7d8f7366514329ed4e6a18d5428972977675537a
billy@arkhamgc.com <billy@arkhamgc.com>
2018-08-12 17:27:07 +0000

    done

--- practice.rb
+++ practice.rb
@@ -1,1 +1,16 @@
 # create a method, called print_array, that takes an argument of an array and uses #each to iterate through the array and puts each element.
+
+
+def print_array (array)
+  array.each {|emement| puts element}
+end
+
+
+
+
+
+
+
+
+
+
