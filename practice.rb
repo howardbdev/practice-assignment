ca84bc73b4ae5de7ba7bd71c27186f764e3e012c
Rajvir Rai <rsr1992@gmail.com>
2018-08-12 17:18:37 +0000

    finish solution to practice problem

--- practice.rb
+++ practice.rb
@@ -1,1 +1,9 @@
 # create a method, called print_array, that takes an argument of an array and uses #each to iterate through the array and puts each element.
+require "pry"
+
+def print_array(array)
+  array.each {|element| puts "The current element is: #{element}"}
+end
+
+binding.pry 
+puts "goodbye"
