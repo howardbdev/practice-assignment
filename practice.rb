311f6ba1894496cd995abd5961d069fc8c24e271
Howard DeVennish <howarddevennish@gmail.com>
2018-08-12 17:18:36 +0000

    finish solution to practice problem

--- practice.rb
+++ practice.rb
@@ -1,1 +1,6 @@
 # create a method, called print_array, that takes an argument of an array and uses #each to iterate through the array and puts each element.
+
+
+def print_array(array)
+  array.each {|element| puts element}
+end 
