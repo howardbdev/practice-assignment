a5f2faea1a4a2d2a42250bfa149410d81f558888
andy.chadha2@gmail.com <andy.chadha2@gmail.com>
2018-08-12 17:18:37 +0000

    Finished Solutions to Practice Problem

--- practice.rb
+++ practice.rb
@@ -1,1 +1,12 @@
 # create a method, called print_array, that takes an argument of an array and uses #each to iterate through the array and puts each element.
+
+animal = ["dog", "cat", "frog", "chicken"]
+def print_array(animal)
+  i = 0
+  while i < animal.length
+  puts "The farmer has a #{animal}"
+  i += 1
+end
+end
+
+print_array(animal)
