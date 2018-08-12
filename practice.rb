372fbc88b25aa69947f8f60c4c846b89dcc0a146
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-12 17:26:40 +0000

    done

--- practice.rb
+++ practice.rb
@@ -1,1 +1,26 @@
 # create a method, called print_array, that takes an argument of an array and uses #each to iterate through the array and puts each element.
+require 'pry'
+
+array = []
+def print_array(array)
+  array.each {|element| puts element
+  
+  
+  
+  
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+binding.pry
