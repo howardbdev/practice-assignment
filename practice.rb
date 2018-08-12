6a2ae0cc4295ab6f170f31b587ddd13ad2b70d3f
alexanderchao123 <chao.alexander.123@gmail.com>
2018-08-12 17:18:40 +0000

    finish solution to practice problem

--- practice.rb
+++ practice.rb
@@ -1,1 +1,11 @@
 # create a method, called print_array, that takes an argument of an array and uses #each to iterate through the array and puts each element.
+
+
+def print_array(array)
+  array.each {|element| puts element}
+end
+
+
+# Test case
+print_array([1, 2, 3, 4, 5])
+print_array(["China", "Taiwan", "America", "Canada", "France"])
